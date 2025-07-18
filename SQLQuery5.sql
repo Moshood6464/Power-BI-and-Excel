ba31a0d843fd95843d5714037d3e13722f8867ab
Moshood Taiwo Olatunde <62583269+Moshood6464@users.noreply.github.com>
2025-07-18 06:53:13 +0100

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,17 @@
+--Insert into EmployeeDemographics VALUES
+--(1002,  'Pam',  'Beasley', 30, 'Female'),
+--(1003,  'Dwight','Schrute', 29, 'Male'),
+--(1004,  'Angela', 'Martin', 28, 'Female'),
+--(1005,   'Joy', 'Scott', 25, 'Female'),
+--(1006,    'Meredith', 'Angela', 24, 'Female'),
+--(1007,    'Ade',   'Ayo', 23, 'Male')
+--(1008,	  'Asake',  'Ade', 20, 'Male'),
+--(1009,	  'Taiwo',   'Adijat', 19, 'Female')
+
+INSERT INTO EmployeeSalary VALUES
+(1002, 'Receptionist',  45000),
+
+
+
+
+
---
+++ SQLQuery9.sql
@@ -0,0 +1,7 @@
+Select *
+FROM  [SQL TUTORIAL].dbo.EmployeeDemographics
+
+Select   *
+From [SQL TUTORIAL].dbo.EmployeeSalary
+
+
