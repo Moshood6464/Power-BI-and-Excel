9bed454ebecd782aa9441d0c97b696bed6dab3f6
Moshood Taiwo Olatunde <62583269+Moshood6464@users.noreply.github.com>
2026-05-03 19:08:04 +0100

    Add files via upload

--- Employee_Demographic.sql
+++ Employee_Demographic.sql
@@ -1,12 +1,21 @@
 use my_first_db;
 show tables;
-select * from employee_demographics;
--- 1. Filtering by Age
-select first_Name, last_Name, Age, gender
-from employee_demographics where Age=30;
--- 2. Grouping  and Counting 
- select gender, count(*) as Total_employees
- from employee_demographics group by Gender;
- 
-
-
+-- 1. Filtering by age
+select first_name, last_name, Age, Gender
+from employee_demographics where age =30;
+-- 2.  Grouping and Counting 
+  select gender, count(*) as total_employees;
+-- 3. Calculating average age
+ select avg(age) as average_Age
+ from employee_demographics
+ order by average_age desc;
+ -- 4. Sorting Employees
+ select employee_id, first_name,last_name,Age
+ from employee_demographics
+ order by age desc;
+      
+  
+      
+      
+  
+  
